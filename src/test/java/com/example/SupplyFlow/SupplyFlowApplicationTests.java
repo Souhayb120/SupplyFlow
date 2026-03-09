281ee0cd08b8ed25a4e75ec37f7bc5a107bf80ed
Souhayb120 <souhaybhadi06@gmail.com>
2026-03-09 10:39:26 +0000

    first commit

---
+++ src/test/java/com/example/SupplyFlow/SupplyFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SupplyFlow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupplyFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
